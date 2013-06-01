3aaffe657d89253d42c1606d8b963d27182dff5b
Karel Ledru-Mathe <karel@ledrumathe.com>
2013-06-01 17:40:00 -0400

    Make a real menu with links to the different layouts

--- routes.js
+++ routes.js
@@ -2,18 +2,25 @@ var folderToHTML  = require('./lib/folderToHtml')
 
 exports.init = function(app) {
 
+    var documentation = function(layout) {
+        return function (req, res) {
 
-    app.get('/', function (req, res) {
+          folderToHTML.render(__dirname+'/views/home/');
 
-      folderToHTML.render(__dirname+'/views/home/');
+          res.render(layout, {
+              title: "Documentation template"
+            , active: 'home'
+            , menu: folderToHTML.getMenuHTML()
+            , html: folderToHTML.getSectionsHTML()
+          })
+        }
+    }
 
-      res.render('two_col_nav', {
-          title: "Documentation template"
-        , active: 'home'
-        , menu: folderToHTML.getMenuHTML()
-        , html: folderToHTML.getSectionsHTML()
-      })
-    });
+    app.get('/', documentation('two_col_nav'));
+
+    app.get('/layout/one-col', documentation('one_col'));
+    app.get('/layout/one-col-nav', documentation('one_col_nav'));
+    app.get('/layout/two-col', documentation('two_col'));
 
 
 }
